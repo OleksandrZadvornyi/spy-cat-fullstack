25ac21807212b32eeb8345bf22d52ff1543368b5
Oleksandr Zadvornyi <zadvornyi.alex16@gmail.com>
2026-02-09 20:22:26 +0200

    feat: support DATABASE_URL env and Postgres URI

--- server/main.py
+++ server/main.py
@@ -5,10 +5,19 @@ import requests
 from sqlalchemy import create_engine, Column, Integer, String, Boolean, ForeignKey, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship, Session
+import os
 
 # --- DATABASE SETUP ---
-SQLALCHEMY_DATABASE_URL = "sqlite:///./spy_cat.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./spy_cat.db")
+
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
